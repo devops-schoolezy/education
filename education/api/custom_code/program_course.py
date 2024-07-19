617205d9d85b2212502c2f7f54e8928398162f9a
Ubuntu <ubuntu@ip-172-26-7-153.ap-south-1.compute.internal>
2024-07-19 11:49:38 +0000

    course code fixed!

--- education/api/custom_code/program_course.py
+++ education/api/custom_code/program_course.py
@@ -2,7 +2,21 @@ import frappe
 
 @frappe.whitelist()
 def get_course(program):
-    course_list = frappe.get_all("Program Course", 
-                                fields=["course"],
-                                filters = {"parent" : program})
+    course_list = []
+    #course_list = frappe.get_all("Program Course", 
+    #                            fields=["course"],
+    #                            filters = {"parent" : program})
+    try:
+        course_list = frappe.db.sql("""
+                                select course
+                                from 
+                                `tabProgram Course` 
+                                where 
+                                parent = %s
+                                """, 
+                                (program),
+                                    as_dict=1,
+                                )
+    except Exception as e:
+        frappe.msgprint(e)   
     return course_list
