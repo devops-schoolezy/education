c752b613b4957eb5141371f626b7ed7607ca6402
Ubuntu <ubuntu@ip-172-26-7-153.ap-south-1.compute.internal>
2024-07-30 10:56:54 +0000

    essl sync data fix!

--- education/api/remote_essl.py
+++ education/api/remote_essl.py
@@ -30,9 +30,13 @@ def get_essl_data():
         # if len(device_id_list_str) != 0:
         #     device_id_list_str += ','
         # device_id_list_str += f"'{id.device_id}'"
+        
         current_datetime = now()
+        lastSynchedTime = getLastSynchedTime(id.shift_type)
+        if len(lastSynchedTime) == 0:
+            lastSynchedTime = current_datetime
 
-        sql_query = f"SELECT dlp.DeviceLogId, dlp.DownloadDate, dlp.DeviceId, dlp.UserId, dlp.LogDate, dlp.Direction, emp.EmployeeCode FROM DeviceLogs_Processed dlp, Employees emp WHERE dlp.DeviceId = {id.device_id} AND dlp.LogDate > '{id.last_synched}' AND dlp.UserId = emp.EmployeeCodeInDevice"
+        sql_query = f"SELECT dlp.DeviceLogId, dlp.DownloadDate, dlp.DeviceId, dlp.UserId, dlp.LogDate, dlp.Direction, emp.EmployeeCode FROM DeviceLogs_Processed dlp, Employees emp WHERE dlp.DeviceId = {id.device_id} AND dlp.LogDate > '{lastSynchedTime}' AND dlp.UserId = emp.EmployeeCodeInDevice"
         logger.debug(sql_query)
         
         # Example query to fetch attendance data
@@ -43,6 +47,7 @@ def get_essl_data():
 
         # Convert each tuple to a dictionary
         json_data = []
+        device_user_ids = []
         for row in attendance_data:
             # Replace column names with actual names from your query
             row_dict = {"DeviceLogId": row[0], "DownloadDate": row[1],  
@@ -50,30 +55,48 @@ def get_essl_data():
                         "LogDate": row[4], "Direction": row[5],
                         "EmployeeCode": row[6]}
             json_data.append(row_dict)
+            device_user_ids.append(row[3])
 
         if json_data:
             # Convert the list of dictionaries to JSON string
             final_json = json.dumps(json_data, default=datetime_handler)
             # Load the JSON string into a Python object
             data = json.loads(final_json)
+            device_ids_mapped_to_emp_ids = []
 
             if isinstance(data, list):
+                device_ids_mapped_to_emp_ids = frappe.db.sql(
+                    """select
+                        name, employee, attendance_device_id from tabEmployee
+                    where
+                        attendance_device_id in (%s)
+                    """
+                    % (", ".join(["%s"] * len(device_user_ids))),
+                )
+
                 for attendance in data:
-                    doc = frappe.new_doc("Employee Checkin")
-                    doc.employee = attendance["EmployeeCode"]
-                    doc.time = attendance["LogDate"]
-                    doc.device_id = attendance["DeviceId"]
-                    doc.log_type = attendance["Direction"].upper()
-                    #doc.skip_auto_attendance = "1"
-                    doc.insert()
+                    employee_id = ""
+                    for emp_detail in device_ids_mapped_to_emp_ids:
+                        if (emp_detail.attendance_device_id == attendance["UserId"]):
+                            employee_id = emp_detail.name
+                            break
+
+                    if len(employee_id) > 0 :
+                        doc = frappe.new_doc("Employee Checkin")
+                        doc.employee = employee_id
+                        doc.time = attendance["LogDate"]
+                        doc.device_id = attendance["UserId"]
+                        doc.log_type = attendance["Direction"].upper()
+                        #doc.skip_auto_attendance = "1"
+                        doc.insert()
             else:
                 logger.debug("Unexpected JSON format. Expected a list of dictionaries.")
 
             #Update the last_synched date with current datetime.
             frappe.db.sql("""
-                        UPDATE `tabEssl` SET last_synched = %s WHERE device_id = %s
+                        UPDATE `tabShift Type` SET last_sync_of_checkin = %s WHERE name = %s
                         """,
-                        (current_datetime, attendance["DeviceId"])
+                        (current_datetime, id.shift_type)
                         )
 
     # Close cursor and connection when done
@@ -92,12 +115,28 @@ def datetime_handler(x):
 def getDeviceList():
     try:
         deviceLists = frappe.db.sql("""
-                                    select device_id, last_synched
+                                    select device_id, shift_type
                                     from 
                                     `tabEssl` 
                                 """,
                                 as_dict=1
                                 )
         return deviceLists
+    except Exception as e:
+        return e
+
+def getLastSynchedTime(shiftName):
+    try:
+        lastSynchedTime = frappe.db.sql("""
+                                    select name, last_sync_of_checkin
+                                    from 
+                                    `tabShift Type` 
+                                    where 
+                                    name = %s
+                                """,
+                                (shiftName),
+                                as_dict=1
+                                )
+        return lastSynchedTime
     except Exception as e:
         return e
